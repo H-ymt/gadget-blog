11232ad024123d90f1ac0aa4297b6d50d8d8d660
H-ymt <y.handai1272@gmail.com>
2024-02-27 16:08:24 +0900

    feat(header): Implementing header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "src/styles/_breakpoint.scss";`,
-        },
-      },
-    },
+          additionalData: `@import "src/styles/_breakpoint.scss";`
+        }
+      }
+    }
   },
+  integrations: [icon()]
 });
